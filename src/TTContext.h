114e1dda00d0d9793253f57814489f7a138ee848
Lucas Saragosa <62749084+LucasSaragosa@users.noreply.github.com>
2021-03-03 14:55:17 +0000

    Update for v2.5.4

--- src/TTContext.h
+++ src/TTContext.h
@@ -5,7 +5,7 @@
 #include "ByteStream.h"
 #include "MetaStream.h"
 #include "Vers.h"
-#include "DCArray.h"
+#include "HandleObjectInfo.h"
 #include "TTArchive.h"
 
 typedef struct Block {
@@ -14,28 +14,34 @@ typedef struct Block {
 } Block;
 
 struct Vers;
+class MetaStream;
+template<typename T>
+class DCArray;
 
 class TTContext {
 	protected:
-		void* archive;
+		TTArchiveOrTTArchive2* archive;
 		bytestream* current_stream;
 		byteoutstream* current_ostream;
 		uint32 start_offset;
 		MetaStream* current_meta;
-		char* current_file_name;
+		char* current_file_name; 
+		char* current_gameid;
 	public:
-		TTContext(void* ttarch_or_ttarch2);
-		void NextArchive(void* ttarch_or_ttarch2, bool del);
+		TTContext(const char f[],TTArchiveOrTTArchive2* arc);
+		void NextArchive(const char ID[], TTArchiveOrTTArchive2* arc, bool del);
+		void NextGame(const char ID[]);
 		~TTContext();
 		bool NextStream(bytestream* next, bool del);
 		bool NextWrite(const char file[],byteoutstream* out, bool del);
 		void OverrideNewMeta(MetaStream* stream, bool del);
-		void FinishCurrentWrite(bool del);
+		void FinishCurrentWrite(bool del, bool updateArchive);
 		char* GetCurrentFileName();
 		Block* ReadBlock();
 		void SkipBlock();
 		void SkipString();
 		char* ReadString();
+		char* GetCurrentGameID();
 		void DeleteBlock(Block* blk);
 		char* FindArchiveEntry(uint64 filename_crc);
 		uint32 GetStartOffset();
